9b32e3a9637e796df905edc631b8c01d75853093
weijingchao <13066010060@163.com>
2025-01-10 16:31:19 +0800

    Add deepface YOLO detector and ROS main file gaze_direction_estimate.py

--- l2cs/__init__.py
+++ l2cs/__init__.py
@@ -1,21 +1,22 @@
-from .utils import select_device, natural_keys, gazeto3d, angular, getArch
-from .vis import draw_gaze, render
-from .model import L2CS
-from .pipeline import Pipeline
 from .datasets import Gaze360, Mpiigaze
+from .model import L2CS
+
+# from .pipeline import Pipeline
+from .utils import angular, gazeto3d, getArch, natural_keys, select_device
+from .vis import draw_gaze, render
 
 __all__ = [
     # Classes
-    'L2CS',
-    'Pipeline',
-    'Gaze360',
-    'Mpiigaze',
+    "L2CS",
+    # "Pipeline",
+    "Gaze360",
+    "Mpiigaze",
     # Utils
-    'render',
-    'select_device',
-    'draw_gaze',
-    'natural_keys',
-    'gazeto3d',
-    'angular',
-    'getArch'
+    "render",
+    "select_device",
+    "draw_gaze",
+    "natural_keys",
+    "gazeto3d",
+    "angular",
+    "getArch",
 ]
